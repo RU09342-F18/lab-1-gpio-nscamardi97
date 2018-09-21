940c6737e5df1ada87c19d73e9d0c7058c8380c6
Nick Scamardi <scamardin0@students.rowan.edu>
2018-09-21 16:03:16 -0400

    Multiple Blink

---
+++ Multiple Blink/MSP430F5529/main.c
@@ -0,0 +1,27 @@
+#include <msp430.h> 
+
+int main(void)
+{
+
+    WDTCTL = WDTPW | WDTHOLD;   // Disable the watchdog timer
+    P1DIR |= BIT0;  // Set P1.0 (Red LED) to output direction using OR
+    P4DIR |= BIT7;  // Set P4.7 (Green LED) to output direction using OR
+
+    // Infinite while loop
+    while (1) {
+        unsigned volatile i, j;
+
+        i++;    // increment counter i
+        j++;    // increment counter j
+
+        if(i > 3000){   // Delay for Red LED
+            i = 0;
+            P1OUT ^= BIT0;  // Toggle P1.0 using exclusive-OR
+        }
+
+        if (j > 30000) {    // Delay for Green LED
+            j = 0;
+            P4OUT ^= BIT7;  // Toggle P4.7 using exclusive-OR
+        }
+    }
+}
---
+++ Multiple Blink/MSP430G2553/main.c
@@ -0,0 +1,26 @@
+#include <msp430.h>
+
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+  P1DIR |= BIT0;    // Set P1.0 (Green LED) to output direction using OR
+  P1DIR |= BIT6;    // Set P1.6 (Red LED) to output direction using OR
+
+  for (;;)  // infinite for loop
+  {
+    volatile unsigned int i,j;
+
+    i++;    // increment counter i
+    j++;    // increment counter j
+
+    if (i > 3000){  // Controls blink rate for P1.0 LED
+        i=0;
+        P1OUT ^= BIT0;  // Toggle P1.0 using exclusive-OR
+    }
+
+    if (j > 30000){ // Controls blink rate for P1.6 LED
+        j=0;
+        P1OUT ^= BIT6;  // Toggle P1.6 using exclusive-OR
+    }
+  }
+}
